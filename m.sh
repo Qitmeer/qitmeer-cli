12940b456de22eefc44e715af806d824717626bf
forchain <tony.zhouhj@gmail.com>
2019-06-29 13:43:35 +0400

    typo correction

--- m.sh
+++ m.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/cli
 
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/mac/cli
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/cli.exe 
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/cli.exe
 
 
 zip -j ./build/window.zip ./build/cli.exe
